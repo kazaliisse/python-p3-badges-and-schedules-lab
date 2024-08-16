b4cb428dc35b896a12a4be134de2c47a40b1b49b
noor <noorhafowbare@gmail.com>
2024-08-16 10:13:50 +0300

    assignment complete

--- lib/debug.py
+++ lib/debug.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from conference_badges import (
     badge_maker, batch_badge_creator,
     assign_rooms, printer
@@ -7,3 +5,20 @@ from conference_badges import (
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+    names = ["Arel", "Carol", "Jim", "Pam"]
+    
+    # Test badge_maker function
+    print(badge_maker("Arel"))
+
+    # Test batch_badge_creator function
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    
+    # Test assign_rooms function
+    rooms = assign_rooms(names)
+    for room in rooms:
+        print(room)
+    
+    # Test printer function
+    printer(names)
